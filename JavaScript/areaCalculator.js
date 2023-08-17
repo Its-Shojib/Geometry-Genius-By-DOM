ff21540b1edcdf6c39aa911430d7d5412a1206b4
Its_Shojib <mdshojib922@gmail.com>
2023-08-17 23:28:14 +0600

    All Area Added

--- JavaScript/areaCalculator.js
+++ JavaScript/areaCalculator.js
@@ -2,9 +2,16 @@ function getInput(idName) {
     let inputField = document.getElementById(idName);
     let inputString = inputField.value;
     let inputNumber = parseFloat(inputString);
-    //    console.log(inputNumber);
     inputField.value = "";
-    return inputNumber;
+    if(isNaN(inputNumber)){
+        alert("Please Enter valid Number");
+    }
+    else{
+        return inputNumber;
+    }
+    //    console.log(inputNumber);
+    
+    
 }
 
 function getTriangleArea() {
@@ -23,5 +30,24 @@ function getParallelogramArea() {
     let base = getInput("parallelogramBase");
     let height = getInput("parallelogramHeight");
     let area = (base * height);
+    console.log(area +" cm2");
+}
+function getRhombusArea() {
+    let D1 = getInput("RhombusD1");
+    let D2 = getInput("RhombusD2");
+    let area = (D1 * D2)/2;
+    console.log(area +" cm2");
+}
+function getPentagonArea() {
+    let p = getInput("PentagonP");
+    let b = getInput("PentagonB");
+    let area = (p * b)/2;
+    console.log(area +" cm2");
+}
+function getEllipseArea() {
+    let PI =3.1416;
+    let a = getInput("EllipseA");
+    let b = getInput("EllipseB");
+    let area = (PI * a * b);
     console.log(area +" cm2");
 }
